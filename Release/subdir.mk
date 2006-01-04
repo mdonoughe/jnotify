6abf9c8e669236d358f24655927455f733ed3bb6
Omry Yadan <omry@yadan.net>
2006-01-04 15:04:27 +0000

    created unified build on linux

--- Release/subdir.mk
+++ Release/subdir.mk
@@ -31,10 +31,10 @@ net_contentobjects_jnotify_win32_JNotify_win32.d \
 %.o: $(ROOT)/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	@echo g++ -I"X:\bin\jdk\1.5.0_04\include" -I"X:\bin\jdk\1.5.0_04\include\win32" -O0 -Wall -c -fmessage-length=0 -o$@ $<
-	@g++ -I"X:\bin\jdk\1.5.0_04\include" -I"X:\bin\jdk\1.5.0_04\include\win32" -O0 -Wall -c -fmessage-length=0 -o$@ $< && \
+	@echo i586-mingw32msvc-g++ -I/usr/i586-mingw32msvc/include -I/usr/lib/gcc/i586-mingw32msvc/3.4.4/include/c++/ -I/usr/lib/j2sdk1.5-sun/include/ -I/usr/lib/j2sdk1.5-sun/include/win32 -O0 -Wall -c -fmessage-length=0 -o$@ $<
+	@i586-mingw32msvc-g++ -I/usr/i586-mingw32msvc/include -I/usr/lib/gcc/i586-mingw32msvc/3.4.4/include/c++/ -I/usr/lib/j2sdk1.5-sun/include/ -I/usr/lib/j2sdk1.5-sun/include/win32 -O0 -Wall -c -fmessage-length=0 -o$@ $< && \
 	echo -n $(@:%.o=%.d) $(dir $@) > $(@:%.o=%.d) && \
-	g++ -MM -MG -P -w -I"X:\bin\jdk\1.5.0_04\include" -I"X:\bin\jdk\1.5.0_04\include\win32" -O0 -Wall -c -fmessage-length=0  $< >> $(@:%.o=%.d)
+	i586-mingw32msvc-g++ -MM -MG -P -w -I/usr/i586-mingw32msvc/include -I/usr/lib/gcc/i586-mingw32msvc/3.4.4/include/c++/ -I/usr/lib/j2sdk1.5-sun/include/ -I/usr/lib/j2sdk1.5-sun/include/win32 -O0 -Wall -c -fmessage-length=0  $< >> $(@:%.o=%.d)
 	@echo 'Finished building: $<'
 	@echo ' '
 
