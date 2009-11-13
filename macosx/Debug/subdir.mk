68518f7e94b2db5ea26233db372ddd5a6a217d05
Matthew Donoughe <mdonoughe@gmail.com>
2009-11-13 00:26:02 +0000

    always build for 10.5

--- macosx/Debug/subdir.mk
+++ macosx/Debug/subdir.mk
@@ -18,8 +18,8 @@ net_contentobjects_jnotify_macosx_JNotifyAdapter_MacOSX_JNFile.d \
 %.o: $(ROOT)/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	@echo gcc -I/System/Library/Frameworks/JavaVM.framework/Headers -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -arch ppc -arch ppc64 -arch i386 -arch x86_64 -o $@ $<
-	@gcc -I/System/Library/Frameworks/JavaVM.framework/Headers -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -arch ppc -arch ppc64 -arch i386 -arch x86_64 -o $@ $< && \
+	@echo gcc -I/System/Library/Frameworks/JavaVM.framework/Headers -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -arch ppc -arch ppc64 -arch i386 -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5 -o $@ $<
+	@gcc -I/System/Library/Frameworks/JavaVM.framework/Headers -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -arch ppc -arch ppc64 -arch i386 -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5 -o $@ $< && \
 	echo $(dir $@)"`gcc -MM -MG -P -w -I/System/Library/Frameworks/JavaVM.framework/Headers -O0 -g3 -Wall -c -fmessage-length=0 -fPIC  $<`" > $(@:%.o=%.d)
 	@echo 'Finished building: $<'
 	@echo ' '
